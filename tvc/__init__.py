6a669994c6b3508f977293441e92ee8938dc817c
hostagen <skillpoint0225@gmail.com>
2022-07-26 06:11:04 +0900

    upgrade version.

--- tvc/__init__.py
+++ tvc/__init__.py
@@ -6,7 +6,7 @@ Wrapper for the Tarkov Changes API
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = 'v0.1.2'
+__version__ = 'v0.2.0'
 
 from .ammunition import *
 from .armor import *
