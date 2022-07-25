06b955c7d09a76919486c8ba2fecbcfae59d48cb
hostagen <skillpoint0225@gmail.com>
2022-07-26 03:46:16 +0900

    import all objects.

--- tvc/__init__.py
+++ tvc/__init__.py
@@ -1,4 +1,10 @@
 __version__ = 'v0.1.0'
 
 from .ammunition import *
+from .armor import *
+from .backpack import *
+from .barter import *
+from .food import *
+from .grenade import *
+from .item import *
 from . import utils
