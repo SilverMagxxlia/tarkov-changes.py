a23338177f68c9da7b4338ac681e4c9c393510eb
hostagen <skillpoint0225@gmail.com>
2022-07-26 04:07:09 +0900

    add client.py

---
+++ tvc/client.py
@@ -0,0 +1,58 @@
+import aiohttp
+
+from typing import Dict, Optional, List
+from asyncio import get_event_loop, AbstractEventLoop
+
+from .ammunition import Ammunition
+from .armor import Armor
+from .requester import HTTPRequester
+
+
+class TVCClient:
+
+    def __init__(
+        self,
+        token: str,
+        loop: Optional[AbstractEventLoop] = None,
+        session: aiohttp.ClientSession = aiohttp.ClientSession(),
+    ) -> None:
+        self.loop: AbstractEventLoop = get_event_loop() if loop is None else loop
+        self.__requester: HTTPRequester = HTTPRequester(token=token, session=session, loop=loop)
+        self.token: str = token
+
+        self._clear()
+
+    def _clear(self) -> None:
+        self._ammunition: Dict[str, Ammunition] = {}
+        self._armors: Dict[str, Armor] = {}
+
+    def start(self) -> None:
+        self.loop.run_until_complete(self.load_endpoints())
+
+    async def load_endpoints(self) -> None:
+        self._clear()
+
+        armors = await self.fetch_armor()
+        ammunition = await self.fetch_ammunition()
+
+        for armor in armors:
+            self._armors[armor.name] = armor
+
+        for ammo in ammunition:
+            self._ammunition[ammo.name] = ammo
+
+    async def fetch_ammunition(self, query: str = None) -> List[Ammunition]:
+        data = await self.__requester.get_ammunition(query)
+        return [Ammunition(d) for d in data]
+
+    async def fetch_armor(self, query: str = None) -> List[Armor]:
+        data = await self.__requester.get_armor(query)
+        return [Armor(d) for d in data]
+
+    @property
+    def ammunition(self) -> List[Ammunition]:
+        return list(self._ammunition.values())
+
+    @property
+    def armors(self) -> List[Armor]:
+        return list(self._armors.values())
