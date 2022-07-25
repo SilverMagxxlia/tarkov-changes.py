fc7663a61984e0a412ddf8ab7509df940e181773
hostagen <skillpoint0225@gmail.com>
2022-07-26 06:02:14 +0900

    fix backpack typo.

--- tvc/backpack.py
+++ tvc/backpack.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .types.backpack import Backpack as BackPackPayload
 
-__all__ = ('BackPack',)
+__all__ = ('Backpack',)
 
 
 class Backpack:
