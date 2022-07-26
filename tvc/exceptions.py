6181434911e754eb3411b27cf6caa20e6c749907
hostagen <skillpoint0225@gmail.com>
2022-07-26 14:44:04 +0900

    if response status is 204 return empty list.

--- tvc/exceptions.py
+++ tvc/exceptions.py
@@ -1,11 +1,59 @@
+from __future__ import annotations
+
+from typing import Any, Dict, Union, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from aiohttp import ClientResponse
+
+    try:
+        from requester import Response
+
+        _ResponseType = Union[ClientResponse, Response]
+
+    except ModuleNotFoundError:
+        _ResponseType = ClientResponse
+
+__all__ = (
+    'TVCException',
+    'RequesterException',
+    'InvalidRequest',
+    'NotFound',
+)
+
+
 class TVCException(Exception):
-
-    def __init__(self, code: int, status: str, msg: str, result: str) -> None:
-        self.status: str = status
-        self.msg: str = msg
-        self.result: str = result
-        self.code: int = code
+    pass
 
 
-class InvalidRequest(TVCException):
+class RequesterException(TVCException):
+
+    def __init__(
+        self,
+        response: _ResponseType,
+        message: Optional[Union[str, Dict[str, Any]]]
+    ) -> None:
+        self.response: _ResponseType = response
+        self.status: int = response.status
+
+        self.text: str
+
+        if isinstance(message, dict):
+            self.text = message.get('msg')
+
+        else:
+            self.text = message or ""
+
+        fmt = "{0.status} {0.reason} (error code: {1})"
+
+        if len(self.text):
+            fmt += ": {2}"
+
+        super().__init__(fmt.format(self.response, self.status, self.text))
+
+
+class InvalidRequest(RequesterException):
+    pass
+
+
+class NotFound(RequesterException):
     pass
